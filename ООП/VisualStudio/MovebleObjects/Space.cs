a33ec0a2a4dbf00d16bad6cf8f8b46fc6170db59
CTpeJlok90D <S90Dima@yandex.com>
2022-11-07 09:52:36 +0300

    new changes

---
+++ ООП/VisualStudio/MovebleObjects/Space.cs
@@ -0,0 +1,23 @@
+﻿namespace MovebleObjects
+{
+    internal class Space
+    {
+        private List<Object> _objects = new();
+
+        public Space(List<Object> objects)
+        {
+            _objects = objects;
+        }
+
+        public Image Render(Size size)
+        {
+            Image image = new Bitmap(size.Width, size.Height);
+            Graphics graphics = Graphics.FromImage(image);
+            foreach (Object obj in _objects)
+            {
+                obj.RenderOn(graphics);
+            }
+            return image;
+        }
+    }
+}
---
+++ ООП/VisualStudio/MovebleObjects/Vector2.cs
@@ -0,0 +1,51 @@
+﻿namespace MovebleObjects
+{
+    internal struct Vector2
+    {
+        public int x;
+        public int y;
+
+        public Vector2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector2 up => new Vector2(0,1);
+        public static Vector2 down => new Vector2(0,-1);
+        public static Vector2 right => new Vector2(1,0);
+        public static Vector2 left => new Vector2(-1,0);
+
+        public static Vector2 operator +(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x + b.x, a.y + b.y);
+        }
+        public static Vector2 operator -(Vector2 a, Vector2 b)
+        {
+            return new Vector2(a.x - b.x, a.y - b.y);
+        }
+        public static bool operator == (Vector2 a, Vector2 b)
+        {
+            return a.x == b.x && a.y == b.y;
+        }
+        public static bool operator !=(Vector2 a, Vector2 b)
+        {
+            return a.x != b.x || a.y != b.y;
+        }
+
+        public static Vector2 operator *(Vector2 a, int b)
+        {
+            return new Vector2(a.x * b, a.y * b);
+        }
+
+        public static Vector2 operator /(Vector2 a, int b)
+        {
+            return new Vector2(a.x / b, a.y / b);
+        }
+
+        public override string ToString()
+        {
+            return $"{x} {y}";
+        }
+    }
+}
